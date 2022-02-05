c0c6c5fbe7d8387248234743a9e2a000000c3b73
Miss Karla Ileana Vega Rios <89174547+karlaavegga@users.noreply.github.com>
2022-02-04 17:09:21 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -12,8 +12,9 @@ function preload (){
 function setup (){
     createCanvas(1900,950);
 
-    //Jugador
-    p1 = createSprite(200,850);
+    //Jugador 
+    p1 = createSprite(3700,850);
+    p1.shapeColor="cyan"
 
     //Plataformas del juego
     g1= createSprite(950,950,1900,50)
@@ -22,7 +23,26 @@ function setup (){
     g2 = createSprite (900,800,200,300);
     g2.shapeColor="#5c1900";
 
+    g3= createSprite(3400,950,800,50)
+    g3.shapeColor="#5c1900";
 
+    g4 = createSprite (1350,500,200,50);
+    g4.shapeColor="#5c1900";
+
+    g5= createSprite(2100,400,300,50)
+    g5.shapeColor="#5c1900";
+
+    g6 = createSprite (3600,525,50,810);
+    g6.shapeColor="#5c1900";
+
+    g7 = createSprite (3300,750,180,30);
+    g7.shapeColor="#5c1900";
+
+    g8 = createSprite (3300,450,550,30);
+    g8.shapeColor="#5c1900";
+
+    g9 = createSprite (3230,135,700,30);
+    g9.shapeColor="#5c1900";
 
 
 }
@@ -49,23 +69,38 @@ function draw (){
 
     if (keyDown("LEFT")){
         p1.x -= 13
-    }
-
-
-        
+    }  
 
     p1.velocityY += 0.8
+    
+    
+    
+    //console.log(mouseX);
+    //console.log(mouseY);
+    console.log("X: " + p1.x);
+    console.log("MouseY: " + mouseY);
+
+    //Colisiones de p1
     p1.collide(g1,jump)
     p1.collide(g2,jump)
-    console.log(p1.collide)
-    drawSprites();
+    p1.collide(g3,jump)
+    p1.collide(g4,jump)
+    p1.collide(g5,jump)
+    p1.collide(g6)
+    p1.collide(g7,jump)
+    p1.collide(g8,jump)
+    p1.collide(g9,jump)
 
+    drawSprites();
 
 }
 
 function jump(){
     if (keyDown("space")){
-        p1.velocityY = p1.velocityY -17
+        
+        if(p1.velocityY > -17   &&  p1.velocityY <17){
+            p1.velocityY = p1.velocityY -17    
+        }
     }
     
 }
